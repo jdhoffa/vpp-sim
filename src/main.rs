be9b738b027e6ba9ef9410672ac43aef246dc3e1
Jackson Hoffart <jackson.hoffart@gmail.com>
2025-09-29 23:38:25 +0100

    feat(battery): log battery charge/ discharge and SOC in main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod devices;
 mod sim;
 
-use devices::{BaseLoad, Device, DeviceContext, SolarPv};
+use devices::{BaseLoad, Battery, Device, DeviceContext, SolarPv};
 use sim::clock::Clock;
 
 fn main() {
@@ -30,18 +30,56 @@ fn main() {
 
     let solar_device = pv.device_type();
 
+    let mut battery = Battery::new(
+        10.0,          /* capacity_kwh */
+        0.5,           /* initial_soc */
+        5.0,           /* max_charge_kw */
+        5.0,           /* max_discharge_kw */
+        0.95,          /* eta_c */
+        0.95,          /* eta_d */
+        steps_per_day, /* steps_per_day */
+    );
+
+    let battery_device = battery.device_type();
+
     clock.run(|t| {
-        let context = DeviceContext {
+        let base_context = DeviceContext {
             timestep: t,
             setpoint_kw: None,
         };
 
-        let base_demand_kw = load.power_kw(&context);
-        let solar_kw = pv.power_kw(&context);
+        let solar_context = DeviceContext {
+            timestep: t,
+            setpoint_kw: None,
+        };
+
+        let base_demand_kw = load.power_kw(&base_context);
+        let solar_kw = pv.power_kw(&solar_context);
+
+        // Simple battery control strategy:
+        // - If solar excess (negative net load), charge battery with excess
+        // - If net load positive, discharge battery to meet load, up to max discharge
+        let net_without_battery = base_demand_kw + solar_kw;
+
+        let battery_context = DeviceContext {
+            timestep: t,
+            setpoint_kw: Some(-net_without_battery), // Negative to charge, positive to discharge
+        };
+
+        let battery_kw = battery.power_kw(&battery_context);
+        let net_with_battery = net_without_battery + battery_kw;
 
         println!(
-            "Timestep {}: {}={:.2}kW, {}={:.2}kW",
-            t, baseload_device, base_demand_kw, solar_device, solar_kw
+            "Timestep {}: {}={:.2}kW, {}={:.2}kW, {}={:.2}kW (SoC={:.1}%), Net={:.2}kW",
+            t,
+            baseload_device,
+            base_demand_kw,
+            solar_device,
+            solar_kw,
+            battery_device,
+            battery_kw,
+            battery.soc * 100.0,
+            net_with_battery
         );
         // later: push `kw` into feeder aggregator
     })
