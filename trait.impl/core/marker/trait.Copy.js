125cd315befbf06aa3236cceea56cadb2a72248c
jdhoffa <jdhoffa@users.noreply.github.com>
2026-02-20 09:36:15 +0000

    deploy: c7b39be911b126edba08e47205d7c4f8bd7bb49c

---
+++ trait.impl/core/marker/trait.Copy.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["vpp_sim",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"vpp_sim/forecast/struct.NaiveForecast.html\" title=\"struct vpp_sim::forecast::NaiveForecast\">NaiveForecast</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"vpp_sim/sim/controller/struct.NaiveRtController.html\" title=\"struct vpp_sim::sim::controller::NaiveRtController\">NaiveRtController</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"vpp_sim/sim/event/struct.DemandResponseEvent.html\" title=\"struct vpp_sim::sim::event::DemandResponseEvent\">DemandResponseEvent</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"vpp_sim/sim/schedule/struct.DayAheadSchedule.html\" title=\"struct vpp_sim::sim::schedule::DayAheadSchedule\">DayAheadSchedule</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1221]}
