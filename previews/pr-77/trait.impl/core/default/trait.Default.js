de0e2db6c37fad4445cd90103e879cf66296d96e
jdhoffa <jdhoffa@users.noreply.github.com>
2026-03-09 10:04:35 +0000

    deploy: f8672f5871b6c244755e78562202d1af19ca5957

---
+++ previews/pr-77/trait.impl/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["vpp_sim",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"vpp_sim/forecast/struct.NaiveForecast.html\" title=\"struct vpp_sim::forecast::NaiveForecast\">NaiveForecast</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"vpp_sim/scenario/struct.ScenarioConfig.html\" title=\"struct vpp_sim::scenario::ScenarioConfig\">ScenarioConfig</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"vpp_sim/sim/controller/struct.NaiveRtController.html\" title=\"struct vpp_sim::sim::controller::NaiveRtController\">NaiveRtController</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"vpp_sim/sim/schedule/struct.DayAheadSchedule.html\" title=\"struct vpp_sim::sim::schedule::DayAheadSchedule\">DayAheadSchedule</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[1255]}
